920bef7f4c7de12c1af3eefc84c1e18c2cd247de
Lauren Zanini <zaninil@mcmaster.ca>
2020-02-14 20:40:07 +0000

    merge

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -11,25 +11,24 @@ fi
 # finds files with #FIXME, puts files in list separated by newline
 if [ "$1" = "fix" ] ; then
 	:> fixme.log
-	find ~/private/CS1XA3 -type f -print0 | while IFS= read -d '' file 
+	find .. -type f -print0 | while IFS= read -d '' file 
 	do
 		tail -n 1 $file | grep -q "#FIXME"
 		if [ "$?" -eq 0 ] ; then
 			echo "$(basename $file)" >> fixme.log
 		fi
 	done
-	
-	cat fixme.log
+
 
 # finds the most recent commit with word 'merge', checks out commit automatically
 elif [ "$1" = "merge" ] ; then
         gitlog=$(git log -1 --oneline | grep -i 'merge')
-	echo $gitlog 
+	echo $gitlog
 
 # prompts user for extension, outputs number of files in repo with said extension
 elif [ "$1" = "file-count" ] ; then
 	read -p "enter file extension to search for: " ext
-	ls -lR ~/private/CS1XA3/ | grep ".$ext" |  wc -l
+	ls -lR .. | grep ".$ext" |  wc -l
 
 # prints invalid input when something is inputted that is not defined
 else
